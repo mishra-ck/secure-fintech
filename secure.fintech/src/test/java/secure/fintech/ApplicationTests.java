41c1e4ffe7f9d19eb30b97ab3555ab6dd5e839ba
Chanchal Mishra <chanchalimsec@gmail.com>
2026-04-08 23:55:19 +0530

    Created project structure

---
+++ secure.fintech/src/test/java/secure/fintech/ApplicationTests.java
@@ -0,0 +1,13 @@
+package secure.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
