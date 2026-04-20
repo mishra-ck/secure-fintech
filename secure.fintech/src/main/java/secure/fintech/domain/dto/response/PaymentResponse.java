802d7279fe7109447dd9b4c1310f5e2325c08dbf
Chanchal Mishra <chanchalimsec@gmail.com>
2026-04-20 23:09:55 +0530

    Payment Response

--- secure.fintech/src/main/java/secure/fintech/domain/dto/response/PaymentResponse.java
+++ secure.fintech/src/main/java/secure/fintech/domain/dto/response/PaymentResponse.java
@@ -2,9 +2,30 @@ package secure.fintech.domain.dto.response;
 
 import lombok.Builder;
 import lombok.Data;
+import secure.fintech.domain.entity.payment.PaymentStatus;
+import secure.fintech.domain.entity.payment.PaymentType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @Builder
 public class PaymentResponse {
-    /*TODO*/
+    private UUID id;
+    private String reference;
+    private BigDecimal amount;
+    private String currency;
+    private String beneficiaryNameMasked;
+    private String beneficiaryIbanMasked;
+    private String beneficiaryBankBic;
+    private PaymentType paymentType;
+    private String referenceMessage;
+    private PaymentStatus status;
+    private LocalDate scheduledDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime approvedAt;
+    private String approvalNotes;
+    private String failureReason;
 }
