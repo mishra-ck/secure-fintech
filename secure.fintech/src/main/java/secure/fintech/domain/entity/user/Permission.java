75ee2efe39124731db04f3c1cccde37619be6c56
Chanchal Mishra <chanchalimsec@gmail.com>
2026-04-21 23:24:03 +0530

    Permissions Entity updated

--- secure.fintech/src/main/java/secure/fintech/domain/entity/user/Permission.java
+++ secure.fintech/src/main/java/secure/fintech/domain/entity/user/Permission.java
@@ -1,4 +1,31 @@
 package secure.fintech.domain.entity.user;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "permissions")
+@Getter @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    @Column(length = 50)
+    private String resource;
+
+    @Column(length = 50)
+    private String action;
+
+    @Column(length = 50)
+    private String description;
 }
