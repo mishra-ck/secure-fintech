c90554e88c1ffaf1cd71740cf39d29e910867dd2
Chanchal Mishra <chanchalimsec@gmail.com>
2026-04-24 23:30:34 +0530

    RateLimiting filter file added

---
+++ secure.fintech/src/main/java/secure/fintech/security/filter/RateLimitingFilter.java
@@ -0,0 +1,13 @@
+package secure.fintech.security.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * Rate Limiting Filter
+ */
+@Component
+@Slf4j
+public class RateLimitingFilter {
+
+}
