ba5eccd882af017a7b84ff7aa226746a75c56c16
Chanchal Mishra <chanchalimsec@gmail.com>
2026-05-06 23:54:08 +0530

    Authentication logic of login

--- secure.fintech/src/main/java/secure/fintech/service/AuthService.java
+++ secure.fintech/src/main/java/secure/fintech/service/AuthService.java
@@ -2,18 +2,38 @@ package secure.fintech.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import secure.fintech.domain.dto.request.LoginRequest;
 import secure.fintech.domain.dto.request.RefreshRequest;
 import secure.fintech.domain.dto.response.MfaSetupResponse;
 import secure.fintech.domain.dto.response.TokenResponse;
+import secure.fintech.domain.entity.user.User;
+import secure.fintech.security.CustomUserDetails;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class AuthService {
+    private final AuthenticationManager authenticationManager;
+    @Transactional
     public TokenResponse login(LoginRequest request, String ip, String userAgent) {
-        /*TODO*/
+        try {
+            // Step : Authenticate Credentials
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+            );
+            CustomUserDetails userDetails = (CustomUserDetails) auth.getPrincipal();
+            User user = userDetails.getUser();
+            /*TODO*/
+
+        }catch(AuthenticationException ex){
+
+        }
         return null;
     }
 
